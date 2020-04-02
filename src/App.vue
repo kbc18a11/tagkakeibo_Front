2d074437b1f5940aef381b0ad40e30ae3a4996d6
Naoumi1214 <wada121400@gmail.com>
2020-04-02 21:31:08 +0900

    bootstrap-vueを導入

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
+      !
     </div>
     <router-view />
   </div>
@@ -30,3 +31,11 @@
   color: #42b983;
 }
 </style>
+<script>
+// @ is an alias to /src
+
+
+export default {
+  components: {}
+};
+</script>
