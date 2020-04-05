5897eb0afd9272191dea97e81881347c694a6256
Naoumi1214 <wada121400@gmail.com>
2020-04-05 17:48:33 +0900

    ユーザー情報や認証トークンに関する格納や取り出しを実装

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+
 import Vue from "vue";
 import Vuex from "vuex";
 
@@ -7,13 +8,69 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     nowlogin: false,//ログインしてるか？
+    name: null,
+    email: null,
+    icon: null,
+    Authorization: null,//認証用トークン
   },
-  mutations: {},
+  mutations: {
+    /**
+     * ログイン開始
+     * @param {*} state 
+     */
+    setLogin(state) {
+      state.nowlogin = true;
+    },
+    /**
+     * ログイン開始
+     * @param {*} state 
+     */
+    setLogout(state) {
+      state.nowlogin = false;
+    },
+    /**
+     * ユーザー情報の格納
+     * @param {*} state 
+     * @param {object} payload 
+     */
+    setUserInfo(state, payload) {
+      state.name = payload.name;
+      state.email = payload.email;
+      state.icon = payload.icon;
+    },
+    /**
+     * トークンの格納
+     * @param {*} state 
+     * @param {object} payload
+     */
+    setAuthorization(state, payload) {
+      state.Authorization = 'Bearer '+ payload.access_token;
+    }
+  },
   actions: {},
   modules: {},
   getters: {
+    /**
+     * ログインのチェック
+     * @param {*} state 
+     */
     checklogin(state) {
       return state.nowlogin;
+    },
+    /**
+     * トークンの取得
+     * @param {*} state 
+     */
+    getAuthorization(state) {
+      return state.Authorization;
+    },
+    /**
+     * ユーザー情報の取得
+     * @param {*} state 
+     */
+    getUserInfo(state) {
+      return [state.name, state.email, state.icon];
     }
+
   }
 });
