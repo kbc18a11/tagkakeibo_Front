fe80b0fb26eea1ba51c6cf0071c9f2e39f8e9c05
Naoumi1214 <wada121400@gmail.com>
2020-04-05 20:14:47 +0900

    ユーザー情報の閲覧と更新するページの追加

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
-
+import UserUpdate from '@/views/UserUpdate.vue'
 
 
 Vue.use(VueRouter);
@@ -23,6 +23,11 @@ const routes = [
     path: "/register",
     name: "Register",
     component: Register
+  },
+  {
+    path: "/userUpdate",
+    name: "UserUpdate",
+    component: UserUpdate
   },
   {
     path: "*",
